6039bdf5f2821d0bb9569c21950f90e46db5a280
Fenrir7Asteron <35568038+Fenrir7Asteron@users.noreply.github.com>
2018-08-29 23:33:45 +0300

    Update ex1.c

--- week2/ex1.c
+++ week2/ex1.c
@@ -1,1 +1,13 @@
-#include <stdio.h>#include <limits.h>#include <float.h>int main(int argc, char *argv[]) {    int x = INT_MAX;    float y = FLT_MAX;    double z = DBL_MAX;    printf("Integer size: %lu\nFloat size: %lu\nDouble size: %lu\n", sizeof(x), sizeof(y), sizeof(z));    printf("Maximum integer: %d\nMaximum float: %f\nMaximum double: %lf\n", x, y, z);}
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main(int argc, char *argv[]) {
+    int x = INT_MAX;
+    float y = FLT_MAX;
+    double z = DBL_MAX;
+
+    printf("Integer size: %lu\nFloat size: %lu\nDouble size: %lu\n", sizeof(x), sizeof(y), sizeof(z));
+    printf("Maximum integer: %d\nMaximum float: %f\nMaximum double: %lf\n", x, y, z);
+}
+
